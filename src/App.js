8628bd7f411253e1e8917b1060927dc4375f5e0b
MB2407 <mann.archna@gmail.com>
2021-07-02 21:04:27 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Navbar from "./components/Navbar.js";
+import "./App.css";
 function App() {
     return(
+
         <div className="app">
+        <Navbar />
             Hey there
         </div>
     );
