9c1ddfde8417fb74c39985a8bd1557b082aa1182
Sayon Sai Dutta <sayon.199303111@muj.manipal.edu>
2021-07-04 22:36:56 +0530

    fixed commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
