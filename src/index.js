704cc4ecd104d354de3caedb97076a52fe7d3509
Sayon Sai Dutta <sayon.199303111@muj.manipal.edu>
2021-07-04 22:35:06 +0530

    again new commit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-
     <React.StrictMode>
-        <App />
+    	<HashRouter>
+        	<App />
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
